98fabe932fd8193f37464ac2df3972bca8e9feef
Gaurav Kumar <gaurav.kumar84@ad.infosys.com>
2020-08-24 23:11:39 +0530

    practice with http

--- inbuilt-package/http.js
+++ inbuilt-package/http.js
@@ -1,8 +1,12 @@
 var http = require('http')
+var port= 5000;
 
 var server = http.createServer(function(req,res){
     res.write('<h1>Hello Node js</h1>')
     res.end()
 })
 
-server.listen(5000);
+server.listen(port,(err)=>{
+    if(err) throw err;
+    console.log(`server is running on port ${port}`);
+});
